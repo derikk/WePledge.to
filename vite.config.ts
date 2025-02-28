691b46a346c40cb2949bd4e931c7a5aaa5cfe512
Derik K <derikk@users.noreply.github.com>
2025-02-27 16:29:28 -0800

    Add Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import type { UserConfig } from "vite";
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), tailwindcss()]
 };
 
 export default config;
